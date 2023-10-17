950e3e96d1e10025dc98e43d888a74ebf9c09b4a
Anthony Rodriguez <aresrtx0518@outlook.es>
2023-10-16 22:05:45 -0400

    Complete Todo Web residence:
    The use of bootstrap has been eliminated
    
    Improved JS code and HTML tags
    
    Vanilla CSS was used

--- app.js
+++ app.js
@@ -1,90 +1,38 @@
-const formulario = document.getElementById('formulario')
-const listaTareas = document.getElementById('lista-tareas')
-const template = document.getElementById('template').content
-const fragment = document.createDocumentFragment()
-let tareas = {}
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-conteiner");
 
-document.addEventListener('DOMContentLoaded', () => {
-    if (localStorage.getItem('tareas')) {
-        tareas = JSON.parse(localStorage.getItem('tareas'))
+function addTask(){
+    if(inputBox.value === ''){
+        alert("Escribe una tarea");
     }
-    pintarTareas()
-})
-
-listaTareas.addEventListener('click', (e) => {btnAccion(e)})
-
-formulario.addEventListener('submit', e => {
-    e.preventDefault()
-    // console.log(e.target[0].value)
-    // console.log(e.target.querySelector('input').value)
-    setTarea(e)
-})
-
-const setTarea = e => {
-    const texto = e.target.querySelector('input').value
-    
-    if (texto.trim() === '') {
-        console.log('está vacio')
-        return
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
     }
-    const tarea = {
-        id: Date.now(),
-        texto: texto,
-        estado: false
-    }
-    
-    tareas[tarea.id] = tarea
-    pintarTareas()
-
-    formulario.reset()
-    e.target.querySelector('input').focus()
+    inputBox.value - "";
+    saveData(); 
 }
 
-const pintarTareas = () => {
-    
-    localStorage.setItem('tareas', JSON.stringify(tareas))
-
-    if (Object.values(tareas).length === 0) {
-        listaTareas.innerHTML = `<div class="alert alert-dark text-center">No Hay tareas pendientes</div>`
-        return
+listContainer.addEventListener('click', function(e){
+    if(e.target.tagname === "LI"){
+        e.target.classlist.toggle("checked");
+        saveData()
     }
-    
-    listaTareas.innerHTML = ''
+    else if(e.target.tagname === "SPAN"){
+        e.target.parentElement.remove();
+        saveData()
+    }
+}, false);
 
-    Object.values(tareas).forEach(tarea => {
-        const clone = template.cloneNode(true)
-        clone.querySelector('p').textContent = tarea.texto
-
-        if (tarea.estado) {
-            clone.querySelectorAll('.fas')[0].classList.replace('fa-check-circle', 'fa-undo-alt')
-            clone.querySelector('.alert').classList.replace('alert-warning', 'alert-primary')
-            clone.querySelector('p').style.textDecoration = 'line-through'
-        }
-
-        clone.querySelectorAll('.fas')[0].dataset.id = tarea.id
-        clone.querySelectorAll('.fas')[1].dataset.id = tarea.id
-        fragment.appendChild(clone)
-    })
-    listaTareas.appendChild(fragment)
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML);
 }
 
-const btnAccion = e => {
-    // console.log(e.target.classList.contains('fa-check-circle'))
-    if (e.target.classList.contains('fa-check-circle')) {
-        tareas[e.target.dataset.id].estado = true
-        pintarTareas()
-    }
-
-    if (e.target.classList.contains('fa-minus-circle')) {
-        // console.log(e.target.dataset.id)
-        delete tareas[e.target.dataset.id]
-        pintarTareas()
-    }
-
-    if (e.target.classList.contains('fa-undo-alt')) {
-        tareas[e.target.dataset.id].estado = false
-        pintarTareas()
-    }
-
-    e.stopPropagation()
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
 }
+showTask();
